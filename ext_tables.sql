fde7ec5a5fce368658870613f9375297d8da3b67
thomas <thomas@example.org>
2011-02-18 20:38:58 +0100

    - change post sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,38 +1,43 @@
 CREATE TABLE tx_tkblog_domain_model_post (
 	uid int(11) NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
-	tstamp int(11) DEFAULT '0' NOT NULL,
-	crdate int(11) DEFAULT '0' NOT NULL,
+	
+	post int(11) unsigned DEFAULT '0' NOT NULL,
+	
+	title varchar(255) DEFAULT '' NOT NULL,
+	author varchar(255) DEFAULT '' NOT NULL,
+	tag_cloud varchar(255) DEFAULT '' NOT NULL,
+	date int(11) DEFAULT '0' NOT NULL,
+	archive int(11) DEFAULT '0' NOT NULL,
+	content int(11) DEFAULT '0' NOT NULL,
+	allow_comments int(11) DEFAULT '0' NOT NULL,
+	views int(11) DEFAULT '0' NOT NULL,
+	category int(11) unsigned DEFAULT '0' NOT NULL,
+	related_post int(11) unsigned DEFAULT '0' NOT NULL,
+
+	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+	crdate int(11) unsigned DEFAULT '0' NOT NULL,
+	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
+	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
+
 	t3ver_oid int(11) DEFAULT '0' NOT NULL,
 	t3ver_id int(11) DEFAULT '0' NOT NULL,
 	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
 	t3ver_label varchar(30) DEFAULT '' NOT NULL,
 	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
-	t3ver_stage tinyint(4) DEFAULT '0' NOT NULL,
+	t3ver_stage int(11) DEFAULT '0' NOT NULL,
 	t3ver_count int(11) DEFAULT '0' NOT NULL,
 	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
 	t3_origuid int(11) DEFAULT '0' NOT NULL,
+
 	sys_language_uid int(11) DEFAULT '0' NOT NULL,
 	l18n_parent int(11) DEFAULT '0' NOT NULL,
 	l18n_diffsource mediumblob NOT NULL,
-	deleted tinyint(1) DEFAULT '0' NOT NULL,
-	hidden tinyint(1) DEFAULT '0' NOT NULL,
-	starttime int(11) DEFAULT '0' NOT NULL,
-	endtime int(11) DEFAULT '0' NOT NULL,
-	fe_group int(11) DEFAULT '0' NOT NULL,
-	title tinytext NOT NULL,
-	author tinytext NOT NULL,
-	date int(11) DEFAULT '0' NOT NULL,
-	archive int(11) DEFAULT '0' NOT NULL,
-	content text NOT NULL,	
-	tagClouds text NOT NULL,
-	category int(11) DEFAULT '0' NOT NULL,
-	related int(11) DEFAULT '0' NOT NULL,
-	allowComments int(11) DEFAULT '0' NOT NULL,
-	numberViews int(11) DEFAULT '0' NOT NULL,
+
 	PRIMARY KEY (uid),
 	KEY parent (pid)
 );
+
 
 CREATE TABLE tt_content (
 	irre_parentid int(11) DEFAULT '0' NOT NULL,
