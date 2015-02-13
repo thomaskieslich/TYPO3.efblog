ad43ad3bdb94d1c9fc3f87719f6c5fca6d049ee8
Thomas Kieslich <info@thomaskieslich.de>
2015-02-13 16:41:08 +0100

    update emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,28 +8,25 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Simple Blog',
-	'description' => 'Blog- Newsextension, based on extbase/fluid.',
-	'category' => 'plugin',
-	'author' => 'Thomas Kieslich',
-	'author_email' => 'info@thomaskieslich.net',
-	'author_company' => '',
-	'shy' => '',
-	'priority' => '',
-	'module' => '',
-	'state' => 'beta',
-	'internal' => '',
-	'uploadfolder' => '0',
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
-	'lockType' => '',
-	'version' => '6.2.0',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '6.2.0-6.2.99',
-		),
-		'conflicts' => array(),
-		'suggests' => array(),
-	)
+		'title' => 'Simple Blog',
+		'description' => 'Blog- Newsextension, based on extbase/fluid.',
+		'category' => 'plugin',
+		'author' => 'Thomas Kieslich',
+		'author_email' => 'info@thomaskieslich.de',
+		'module' => '',
+		'state' => 'stable',
+		'internal' => '',
+		'uploadfolder' => '0',
+		'createDirs' => '',
+		'modify_tables' => '',
+		'clearCacheOnLoad' => 0,
+		'lockType' => '',
+		'version' => '6.2.0',
+		'constraints' => array(
+				'depends' => array(
+						'typo3' => '6.2.0-6.2.999',
+				),
+				'conflicts' => array(),
+				'suggests' => array(),
+		)
 );
