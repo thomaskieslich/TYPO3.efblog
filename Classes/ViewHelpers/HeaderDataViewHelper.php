bb339077953522cc711b8b984166e85ba8f2c9f8
Thomas Kieslich <info@thomaskieslich.de>
2015-02-21 17:51:09 +0100

    add headertag viewhelper for rss link

---
+++ Classes/ViewHelpers/HeaderDataViewHelper.php
@@ -0,0 +1,58 @@
+<?php
+namespace ThomasKieslich\Efblog\ViewHelpers;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2011-2015 Thomas Kieslich
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the text file GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * ViewHelper to render data in <head> section of website
+ *
+ * # Example: Basic example
+ * <code>
+ * <n:headerData>
+ * 		<link rel="alternate"
+ * 			type="application/rss+xml"
+ * 			title="RSS 2.0"
+ * 			href="<f:uri.page additionalParams="{type:9818}"/>" />
+ * </n:headerData>
+ * </code>
+ * <output>
+ * Added to the header: <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="uri to this page and type 9818" />
+ * </output>
+ *
+ */
+class HeaderDataViewHelper extends AbstractViewHelper {
+
+	/**
+	 * Renders HeaderData
+	 *
+	 * @return void
+	*/
+	public function render() {
+		$GLOBALS['TSFE']->getPageRenderer()->addHeaderData($this->renderChildren());
+	}
+}
