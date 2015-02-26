a11f3e1b46094bdfc1cf8bb4ff4af7155542b53b
Thomas Kieslich <info@thomaskieslich.de>
2015-02-26 15:38:40 +0100

    post view ajax to localstore

--- Resources/Public/JavaScript/ajax.js
+++ Resources/Public/JavaScript/ajax.js
@@ -2,36 +2,31 @@ var baseUrl = '?type=911&tx_efblog_fe1[controller]=Ajax';
 
 $(function () {
 	//update Post views
-//	if ($('.tx-efblog-detail-container').length > 0) {
-//		var agent = window.navigator.userAgent;
-//		var pid = $('.tx-efblog').data('pid') + "";
-//		var postUid = $('.tx-efblog-detail-container').data('uid') + "";
-//		var pattern = /bot|googlebot|crawler|spider|robot|crawling/i;
-//
-////		$.removeCookie('txEfblogPostCount');
-//		var postCookie = $.cookie('txEfblogPostCount'),
-//				postArr = postCookie ? postCookie.split('|').getUnique() : [];
-//
-//		if (!pattern.test(agent) && $.inArray(postUid, postArr) == -1) {
-//			$.ajax({
-//				dataType: "text",
-//				url: baseUrl + '&id=' + pid,
-//				data: {
-//					'tx_efblog_fe1[action]': 'updateViews',
-//					'tx_efblog_fe1[post]': postUid
-//				},
-//				success: function (result) {
-//					if (result == "true") {
-//						postArr.push(postUid);
-//						postArr.getUnique();
-//						$.cookie('txEfblogPostCount', postArr.join('|'), {
-//							expires: 1
-//						});
-//					}
-//				}
-//			});
-//		}
-//	}
+	if ($('.tx-efblog-detail-container').length > 0) {
+		var agent = window.navigator.userAgent;
+		var pid = $('.tx-efblog').data('pid') + "";
+		var postUid = $('.tx-efblog-detail-container').data('uid') + "";
+		var pattern = /bot|googlebot|crawler|spider|robot|crawling/i;
+
+		var txEfblogPostViews = (getLocalStorage('txEfblogPostViews')) ? getLocalStorage('txEfblogPostViews') : [];
+
+		if (!pattern.test(agent) && $.inArray(postUid, txEfblogPostViews) == -1) {
+			$.ajax({
+				dataType: "text",
+				url: baseUrl + '&id=' + pid,
+				data: {
+					'tx_efblog_fe1[action]': 'updateViews',
+					'tx_efblog_fe1[post]': postUid
+				},
+				success: function (result) {
+					if (result == "true") {
+						txEfblogPostViews.push(postUid);
+						setLocalStorage('txEfblogPostViews', txEfblogPostViews.unique(), 1440 * 90);
+					}
+				}
+			});
+		}
+	}
 });
 
 //###Calendar
